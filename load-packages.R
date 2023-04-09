9a546d3abc463ba08d6d6e3f1237fb4663e7beb8
Youngrok Lee <cakeil@gmail.com>
2023-04-09 12:44:57 -0400

    explain the purpose of this file

--- load-packages.R
+++ load-packages.R
@@ -1,2 +1,5 @@
+# This file is not required for docker image build.
+# This file has been created just for illustration purpose to let renv.lock 
+# file capture R packages that are used in the project.
 library(dplyr)
 library(devtools)
